2adfc101d98602c4169e5e1ac8acba3228db0482
phanquan0712 <phanhongquan07122003@gmail.com>
2022-07-22 11:37:24 +0700

    update

--- src/utils/FetchData.ts
+++ src/utils/FetchData.ts
@@ -1,37 +1,37 @@
 import axios from 'axios';
-
+import { API_URL } from './config';
 
 
 export const postApi = async(url: string, post: object, token?: string ) => {
-   const res = await axios.post( `/api/${url}`, post, {
+   const res = await axios.post( `${API_URL}/api/${url}`, post, {
       headers: { Authorization: token}
    })
    return res;
 }
 
 export const getApi = async(url: string, token?: string ) => {
-   const res = await axios.get(`/api/${url}`, {
+   const res = await axios.get(`${API_URL}/api/${url}`, {
       headers: { Authorization: token}
    })
    return res;
 }
 
 export const patchApi = async(url: string, post: object, token?: string ) => {
-   const res = await axios.patch( `/api/${url}`, post, {
+   const res = await axios.patch( `${API_URL}/api/${url}`, post, {
       headers: { Authorization: token}
    })
    return res;
 }
 
 export const putApi = async(url: string, post: object, token?: string ) => {
-   const res = await axios.put( `/api/${url}`, post, {
+   const res = await axios.put( `${API_URL}/api/${url}`, post, {
       headers: { Authorization: token}
    })
    return res;
 }
 
 export const delApi = async(url: string, token?: string ) => {
-   const res = await axios.delete( `/api/${url}`, {
+   const res = await axios.delete( `${API_URL}/api/${url}`, {
       headers: { Authorization: token}
    })
    return res;
